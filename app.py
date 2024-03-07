fb033db50d8f7bb8b9a79196b7304aa8b9c27881
Skogis <filip.skoglund07@outlook.com>
2024-03-07 13:03:36 +0100

    renskrev kommentarer

--- app.py
+++ app.py
@@ -7,50 +7,38 @@ a = float(input("Ange a: "))
 b = float(input("Ange b: "))
 c = float(input("Ange c: "))
 
-#kollar nollställen och min/max punkter
-D = b**2 - 4*a*c #Diskriminanten i ekvationen
+# Här kollar vi nollställen och min/max punkter
+D = b**2 - 4*a*c # D står för Diskriminanten
 if D == 0:
     print("Dubbelrot, ett nollställe")
 elif D > 0:
     print("Två nollställen")
     
-    #rötterna
+    # Rötter
     x1 = (-b - np.sqrt(D))/(2*a)
     x2 = (-b + np.sqrt(D))/(2*a)
-    
-    #Min/max punkter
-    xsym = -b/(2*a)
-    if a > 0:
-        minpunkt = a*xsym**2 + b*xsym + c
-        print("Minimipunkt", "(",xsym,",", minpunkt,")")
-    else:
-        maxipunkt = a*xsym**2 + b*xsym + c
-        print("Maximipunkt", "(",xsym,",", maxipunkt,")")
-    print("rötterna är x1=",x1, "och x2=",x2, "Symetrilinjen är Xsym=",xsym,)
-elif D < 0: 
-    print("Inga reealla nollställen")
-    sys.exit()
 
-#Uträkning av andragradsekvationen
-RäknaGraf = input("Vill du räkna med graf? y/n:")
-if RäknaGraf == "y":
-     #modulen för att rita grafer
-    
+# Grafuträkning av andragradsekvationen    
+räknaGraf = input("Vill du räkna med graf? y/n:")
+if räknaGraf == "y":
+     
+    # Funktion som räknar ut Y med respektive variabel
     def f(x, a, b, c):
-        return a * x**2 + b * x + c #Allmäna formen för en andragradsekvation
-        # ax^2 + bx + c
+        return a * x**2 + b * x + c # Allmäna formen (ax^2 + bx + c)
     
-    xlist = np.linspace(-10, 10, 1000) #Skapar en lista med 1000 värden mellan -10 och 10
-    ylist = f(xlist, a, b, c) #Skapar en lista med y-värden för varje x-värde    
+    xlist = np.linspace(-10, 10, 1000) # Skapar en lista med 1000 värden mellan -10 och 10
+    ylist = f(xlist, a, b, c) # Skapar ett y-värde för varje x-värde    
 
+    # Modulen som ritar grafen och självaste grafen
     plt.figure(num=0, dpi=120)
     plt.plot(xlist, ylist, label="f(x)")
     plt.title("Andragradsekvation")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend()
-    plt.grid(True)
-    plt.show()
+    plt.grid(True) # Lägger till ett rutsystem som grafen visas i
+    plt.show() # Denna visar grafen
 else: 
+    # Om vill du räkna med graf = n (nej)
     print("Programmet avslutas")
     sys.exit()
